45a28ba628e7a8b0e525ada86cce33fbd06aa555
Haroon <haroon20@hotmail.co.uk>
2022-01-18 15:51:26 +0000

    Final

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,13 @@
-def fizzbuzz(number)
-  if (number % 3 == 0 && number % 5 == 0)
-    return "fizzbuzz"
-  elsif number % 5 == 0
-    return "buzz"
-  elsif number % 3 == 0
-    return "fizz"
-  else
-    return number
+class Integer
+  def fizzbuzz
+    if (self % 3 == 0 && self % 5 == 0)
+      return "fizzbuzz"
+    elsif self % 5 == 0
+      return "buzz"
+    elsif self % 3 == 0
+      return "fizz"
+    else
+      return self
+    end
   end
 end
